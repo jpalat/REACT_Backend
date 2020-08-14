f1849cee4b7fead4543e2c2bfb535aa4c92d95b7
WaichungLing <weling@ucsd.edu>
2020-08-14 22:38:00 +0800

    auto reload

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql')
+
+const app = express()
+const selectAll = "SELECT DATE_FORMAT(time,'%Y-%m-%d %H:%i:%S') as 'time',voltage,current,SOC,SOHR,SOHC FROM sbg";
+const port = 5000
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'GZc51504',
+    database:'gear'
+})
+
+connection.connect(err=>{
+    if (err){
+        return err;
+    }
+})
+
+// console.log(connection)
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('First time download--> /init')
+})
+
+app.get('/init', (req, res) => {
+    connection.query(selectAll,(err,results)=>{
+        if (err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data:results
+            })
+        }
+    })
+})
+
+app.get('/update',(req,res) => {
+    const obj = req.query;
+    const updateQuery = "SELECT DATE_FORMAT(time,'%Y-%m-%d %H:%i:%S') as 'time',voltage,current,SOC,SOHR,SOHC FROM sbg WHERE time > '"+ obj.time + "'"
+    // console.log(obj)
+    // console.log(updateQuery)
+    connection.query(updateQuery,(err,results)=>{
+        if (err){
+            return res.send(err)
+        }
+        else{
+            return res.json({
+                data:results
+            })
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
